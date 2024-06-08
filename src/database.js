dcf7594687095f734bc0b9fd0ca440aee9146165
charles <charlesadechi@gmail.com>
2024-06-08 04:08:54 +0200

    tenth commit

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('sezydb', 'sezydb_user', 'SvfXGPdXPK46HzmjGnIixilCFbn49abE', {
-  host: 'dpg-cphrd2ect0pc73fihjbg-a',
+  host: 'postgres://sezydb_user:SvfXGPdXPK46HzmjGnIixilCFbn49abE@dpg-cphrd2ect0pc73fihjbg-a.frankfurt-postgres.render.com/sezydb',
   dialect: 'postgres',
   port: 5432, // Par défaut pour PostgreSQL
 });
